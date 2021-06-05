d15135b674ced42ab974797e2b235b7987af6c7c
Inertia123 <78479101+Inertia123@users.noreply.github.com>
2021-06-06 01:31:15 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Note(){
+    return (
+<div className = "note"><h1>Javascript and React,js</h1>
+<p>This was an amazing bootcamp taken up by Shaurya Shinha Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta eos labore odio eum, numquam iusto!</p></div>
+    );
+    
+}
+export default Note;
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <div>
+    <h1>Hi</h1>
+  </div>,
+  document.getElementById("root")
+);
